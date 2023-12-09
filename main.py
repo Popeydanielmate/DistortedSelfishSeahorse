f43d4424ee410ef29fef864870ed19498b58ea48
Popeydanielmate <132638858+Popeydanielmate@users.noreply.github.com>
2023-12-09 08:04:05 +0000

    commit1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import random
 
+
 def get_choices():
-  player_choice = input ("Enter a choice (rock, paper, scissors: ")
+  player_choice = input("Enter a choice (rock, paper, scissors: ")
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
 
+
 def check_win(player, computer):
+  print(f"You chose {player} Computer chose {computer}")
+  if player == computer:
+  return "It's a tie!"
