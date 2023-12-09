7dc674ecc39fb084a92cdee920e66d9753071e5c
Popeydanielmate <132638858+Popeydanielmate@users.noreply.github.com>
2023-12-09 08:12:31 +0000

    commit2

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ def get_choices():
 def check_win(player, computer):
   print(f"You chose {player} Computer chose {computer}")
   if player == computer:
-  return "It's a tie!"
+    return "It's a tie!"
+  elif player == "rock" and computer == "scissors":
+    return "Rock smashes scissors! You win!"
+
+check_win("rock", "paper")
