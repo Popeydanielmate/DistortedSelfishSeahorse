acaf5fdab101de746f4794bda24945873c9747d1
Popeydanielmate <132638858+Popeydanielmate@users.noreply.github.com>
2023-12-11 10:04:23 +0000

    commit4

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 
 def get_choices():
-  player_choice = input("Enter a choice (rock, paper, scissors: ")
+  player_choice = input("Enter a choice (rock, paper, scissors): ")
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
@@ -31,3 +31,4 @@ def check_win(player, computer):
 
 choices = get_choices()
 result = check_win(choices["player"], choices["computer"])
+print(result)
